94bed5c77c7578386104f4b12d44079052c8cdd7
Esther Amponsaa <eamponsaa@gmail.comgit config --global user.name Your>
2018-08-08 15:59:04 -0400

    guess

--- cssi-labs (copy)/python/labs/shopping-list-app/starter-code/shopping_list_starter.py
+++ cssi-labs (copy)/python/labs/shopping-list-app/starter-code/shopping_list_starter.py
@@ -27,7 +27,28 @@ while choice.lower() != "e":
     print("c. Check to see if an item is on the list")
     print("d. Show all items on the list")
     print("e. exit")
-    
-    choice = input("Enter your choice [a|b|c|d|e]:")
-    
+
+    choice = raw_input("Enter your choice [a|b|c|d|e]:")
+    if choice == 'a':
+        item = raw_input("Enter item:")
+        shopping_list.append(item)
+    elif choice == 'b':
+        item = raw_input("Enter item:")
+        shopping_list.remove(item)
+    elif choice == 'c':
+        item = raw_input("Enter item:")
+        if item in shopping_list:
+            print "item is in the list"
+        else:
+            print "item is not in the list"
+    elif choice == 'd':
+        for i in shopping_list:
+            print i
+        print "\n"
+    else:
+        print "please try again"
+        exit()
+
+
+
     # Your code below! Handle the cases when the user chooses a, b, c, d, or e
